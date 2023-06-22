9cd47248fad99fb028c9f37ed4f2fe2514b124cc
alexop345 <alexandru.oprea@rodeapps.com>
2023-06-22 10:18:02 +0300

    added rxdart

--- lib/view_models/counter_view_model.dart
+++ lib/view_models/counter_view_model.dart
@@ -1,20 +1,21 @@
 import 'dart:async';
 
 import 'package:mvvm_practice/models/counter.dart';
+import 'package:rxdart/rxdart.dart';
 
 class CounterViewModel {
   final Counter _counter = Counter();
-  final StreamController<Counter> _streamController = StreamController();
+  final BehaviorSubject<Counter> _behaviorSubject = BehaviorSubject<Counter>();
 
   CounterViewModel() {
-    _streamController.add(_counter);
+    _behaviorSubject.add(_counter);
   }
 
-  Stream get stream => _streamController.stream;
+  Stream get stream => _behaviorSubject.stream;
   Counter get counter => _counter;
 
   void incrementCounter() {
     _counter.value++;
-    _streamController.add(_counter);
+    _behaviorSubject.add(_counter);
   }
 }
