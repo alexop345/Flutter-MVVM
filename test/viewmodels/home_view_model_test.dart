a67bf00d71005d110f0bccf3d09ba94787a45f38
alexop345 <alexandru.oprea@rodeapps.com>
2023-06-26 10:43:40 +0300

    unit tests with mocktail

--- test/viewmodels/home_view_model_test.dart
+++ test/viewmodels/home_view_model_test.dart
@@ -1,23 +1,34 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:mvvm_practice/models/counter.dart';
 import 'package:mvvm_practice/repo/counter_repo.dart';
 import 'package:mvvm_practice/screens/home/home_view_model.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../mocks/repos_mocks.dart';
 
 main() {
   group('Counter tests', () {
-    late CounterRepo counterRepo;
-    late HomeViewModel homeViewModel;
+    late CounterRepo repo;
+    late HomeViewModel viewModel;
     late Subject<bool> onIncrement;
     late Subject<void> onReset;
 
     setUp(() {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      SharedPreferences.setMockInitialValues({});
+
       onIncrement = BehaviorSubject<bool>.seeded(false);
       onReset = PublishSubject<void>();
-      homeViewModel = HomeViewModel(Input(
+      viewModel = HomeViewModel(Input(
         onIncrement,
         onReset,
       ));
+      repo = MockCounterRepo();
     });
 
     tearDown(() {
@@ -28,7 +39,29 @@ main() {
     test(
       'Test stream emits counter with value 0',
       () {
-        expect(homeViewModel.output, isNotNull);
+        when(() => repo.getCounter()).thenAnswer((_) => Stream.value(Counter()));
+        expect(viewModel.input, isNotNull);
+        expect(viewModel.output, isNotNull);
+        viewModel.output.onCountResult.listen(
+          (event) {
+            expect(event.value, 0);
+          },
+        );
+      },
+    );
+
+    test(
+      'Test stream emits counter with value 1',
+      () {
+        when(() => repo.getCounter()).thenAnswer((_) => Stream.value(Counter()));
+        expect(viewModel.input, isNotNull);
+        expect(viewModel.output, isNotNull);
+        viewModel.input.onIncrement.add(true);
+        viewModel.output.onCountResult.listen(
+          (event) {
+            expect(event.value, 1);
+          },
+        );
       },
     );
   });
